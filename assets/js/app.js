d8ac294efaf1161b90cbbe7e06f426550df5a341
Rifars <ramlifr12@gmail.com>
2021-04-10 08:20:57 +0700

    resolve code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-// AOS.init({
-//     duration: 1000
-// });
+AOS.init({
+    duration: 1000
+});
 
 $(document).on("click", '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
